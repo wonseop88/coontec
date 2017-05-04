3a32399a845fdbb1957ed5145149eb20a2ee3d88
coontec <wonseop@coontec.com>
2017-05-04 16:38:30 +0900

    email tamplets

--- coontec/homepage/emailSend.py
+++ coontec/homepage/emailSend.py
@@ -27,16 +27,28 @@ def send_email(request):
         body['From'] = send_email
         body['To'] = send_email
         
-        html = "이름 : " + request.GET['name'] + "<br>"
-        html += "Email : " + request.GET['email'] + "<br>"
-        html += "제목 : " + request.GET['subject'] + "<br>"
-        html += "내용 : <br><div>"+request.GET['message']+"</div>"
+        html = '<div align="center" style="width: 100%;">'
+        html += '<table style="background-color: rgb(255, 255, 255); width: 500px; font-size: 9pt;"><tbody>'
+        html += '<tr style="height: 2px;"><td colspan="3" style="background: rgb(0, 136, 178); height: 2px;"></td></tr>'
+        html += '<tr style="height: 40px;"><td colspan="3" style="background: rgb(241, 241, 241);"><b>&nbsp;안녕하세요 쿤텍(주) 입니다.&nbsp <br>&nbsp;보내주신 문의사항은 다음과 같습니다.</b></td></tr>'
+        html += '<tr style="height: 2px;"><td colspan="3" style="background: rgb(217, 217, 217); height: 2px;"></td></tr>'
+        html += '<tr style="height: 31px;"><td style="width: 200px;"><b>&nbsp;이름 ( Name )</b></td><td>|</td><td>&nbsp;'+request.GET['name']+'</td></tr>'
+        html += '<tr style="height: 1px;"><td colspan="3" style="background: rgb(217, 217, 217); height: 1px;"></td></tr>'
+        html += '<tr style="height: 32px;"><td><b>&nbsp;메일 ( E- mail )</b></td><td>|</td><td>&nbsp;'+request.GET['email']+'</td></tr>'
+        html += '<tr style="height: 1px;"><td colspan="3" style="background: rgb(217, 217, 217); height: 1px;"></td></tr>'
+        html += '<tr style="height: 32px;"><td><b>&nbsp;제목&nbsp;(Subject ) </b></td><td>|</td><td>&nbsp;'+request.GET['name']+'</td></tr>'
+        html += '<tr style="height: 1px;"><td colspan="3" style="background: rgb(217, 217, 217); height: 1px;"></td></tr>'
+        html += '<tr style="height: 32px;"><td colspan="3"><b>&nbsp;내용 ( Message )</b></td></tr>'
+        html += '<tr style="height: 1px;"><td colspan="3" style="background: rgb(217, 217, 217); height: 1px;"></td></tr>'
+        html += '<tr style="height: 50px;"><td colspan="3" style="height: 50px;">&nbsp;'+request.GET['message']+'</td></tr>'
+        html += '<tr style="height: 2px;"><td colspan="3" style="background: rgb(0, 136, 178); height: 2px;"></td></tr>'
+        html += '</tbody></table></div>'
         
         msg = MIMEText(html, 'html')
         body.attach(msg)
     
         server.sendmail(from_addr=send_email,
-                        to_addrs=[send_email],  # list, str 둘 다 가능
+                        to_addrs=[send_email,request.GET['email']],  # list, str 둘 다 가능
                         msg=body.as_string())
     
         server.quit()
